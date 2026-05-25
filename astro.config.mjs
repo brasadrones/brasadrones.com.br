609e68564b5648d7f5befd7b693e43d8b210909f
Beserrovsky <felipebeserra25@gmail.com>
2026-05-25 09:35:00 -0300

    vercel connector

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
@@ -6,6 +7,8 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: vercel(),
   site: "https://astroship.web3templates.com",
   integrations: [mdx(), sitemap(), icon()],
   vite: {
